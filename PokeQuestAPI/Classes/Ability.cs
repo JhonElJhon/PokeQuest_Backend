c1f50dd1085ee65a83e470fb7a315bb065eef836
Esteban2727 <escastr@gmail.com>
2025-04-24 20:10:23 -0500

    Se crean las queries para las habilidades y se crea el controlador Ability

---
+++ PokeQuestAPI/Classes/Ability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PokeQuestAPI.Classes
+{
+    public class Ability
+    {
+        public int ID { get; set; }
+        public string NombreEN { get; set; }
+        public string NombreES { get; set; }
+
+    }
+}
